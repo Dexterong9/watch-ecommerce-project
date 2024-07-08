cd46123169a3a6e75e84033bcdbed9df19c89777
Dexterong9 <dexter.ong6@gmail.com>
2024-07-08 14:28:43 +0800

    Add files via upload

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.service.ProductService;
 
@@ -22,5 +23,17 @@ public class ProductController {
 	public String listAllProducts(Model model) {
 		model.addAttribute("products", productService.getAllProducts());
 		return "products";
+	}
+	
+//	@GetMapping("/product/detail/{id}")
+//	public String showProduct(Model model) {
+//		model.addAttribute("products", productService.getAllProducts());
+//		return "ProductMain";
+//	}
+	
+	@GetMapping("/product/detail/{id}")
+	public String showProductDetails(@PathVariable Long id, Model model) {
+		model.addAttribute("product", productService.getProductById(id));
+		return "ProductMain";
 	}
 }
